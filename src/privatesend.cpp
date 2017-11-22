823d74ff7d2c64cec1593e78c7f6c803ea520312
genavarov <genavarov@gmail.com>
2017-11-22 16:36:57 +0200

    16352211
    BASH Start

--- src/privatesend.cpp
+++ src/privatesend.cpp
@@ -303,10 +303,10 @@ int CPrivateSend::GetDenominations(const std::vector<CTxOut>& vecTxOut, bool fSi
 bool CPrivateSend::GetDenominationsBits(int nDenom, std::vector<int> &vecBitsRet)
 {
     // ( bit on if present, 4 denominations example )
-    // bit 0 - 100DASH+1
-    // bit 1 - 10DASH+1
-    // bit 2 - 1DASH+1
-    // bit 3 - .1DASH+1
+    // bit 0 - 100BASH+1
+    // bit 1 - 10BASH+1
+    // bit 2 - 1BASH+1
+    // bit 3 - .1BASH+1
 
     int nMaxDenoms = vecStandardDenominations.size();
 
--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -38,7 +38,7 @@ class TxViewDelegate : public QAbstractItemDelegate
     Q_OBJECT
 public:
     TxViewDelegate(const PlatformStyle *_platformStyle, QObject *parent=nullptr):
-        QAbstractItemDelegate(parent), unit(BitcoinUnits::DASH),
+        QAbstractItemDelegate(parent), unit(BitcoinUnits::BASH),
         platformStyle(_platformStyle)
     {
 
@@ -275,7 +275,7 @@ void OverviewPage::setWalletModel(WalletModel *model)
     this->walletModel = model;
     if(model && model->getOptionsModel())
     {
-        // update the display unit, to not use the default ("DASH")
+        // update the display unit, to not use the default ("BASH")
         updateDisplayUnit();
         // Keep up to date with wallet
         setBalance(model->getBalance(), model->getUnconfirmedBalance(), model->getImmatureBalance(), model->getAnonymizedBalance(),
